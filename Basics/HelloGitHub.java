f4baf12e40ee52e92bc9960f4e314714b6d87025
streetcoder10 <vikygopro@gmail.com>
2017-07-05 10:23:01 -0400

    Create HelloGitHub.java

---
+++ Basics/HelloGitHub.java
@@ -0,0 +1,13 @@
+/*
+ * Author - Vikram Gopal
+ * Title - Printing out to console.
+ */
+ 
+public class HelloWorld 
+{
+	public static void main (String [] args)
+	{
+		System.out.println("Hello GitHub!!");
+	}
+
+}
