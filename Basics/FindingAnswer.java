4e6d9c0231bf39a6b5bc5c05b664fa9a0aa50c49
streetcoder10 <vikygopro@gmail.com>
2017-07-05 10:27:32 -0400

    Create FindingAnswer.java

---
+++ Basics/FindingAnswer.java
@@ -0,0 +1,19 @@
+/*
+ * Author - Vikram Gopal.
+ * Title - Finding answer using declaring and variables.
+ */
+
+package Viky_Programs;
+
+public class FindingAnswer 
+{
+	public static void main(String [] args)
+	{
+		int value1 = 10;		
+		int value2 = value1+10;
+		double value3 = value1+20-value2;
+		double theAnswer = (value3*10)/10;
+		
+		System.out.println("The answer is: "+theAnswer);
+	}
+}
