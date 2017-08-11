7448fcb2cbe0b67a6fa4c496f33856e63aa49758
Vikram Gopal <vikygopro@gmail.com>
2017-08-11 14:22:39 -0400

    Create HelloGitHub.java

---
+++ Java Programs/HelloGitHub.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+public static void main(String [] args) {
+
+System.out.println("Hello GitHub!");
+System.out.println("My Name is Vikram and I am a developer!");
+
+  }
+}
