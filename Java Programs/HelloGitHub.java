15263687c45db1039763d9a8098c7d3c4c2bc795
Vikram Gopal <vikygopro@gmail.com>
2017-08-13 01:04:03 -0400

    Delete HelloGitHub.java

--- Java Programs/HelloGitHub.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-
-public static void main(String [] args) {
-
-System.out.println("Hello GitHub!");
-System.out.println("My Name is Vikram and I am a developer!");
-
-  }
-}
